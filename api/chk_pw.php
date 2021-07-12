d819d38a3c9a872e7c9957a35b80cd59789652c1
kai <cgk0910@gmail.com>
2021-07-12 16:39:40 +0800

    完成會員登入檢查功能

---
+++ api/chk_pw.php
@@ -0,0 +1,14 @@
+<?php include_once "../base.php";
+
+$acc=$_GET['acc'];
+$pw=$_GET['pw'];
+echo $Mem->count(['acc'=>$acc,'pw'=>$pw]);
+
+/* $chk=$Mem->count(['acc'=>$acc]);
+
+if($chk>0){
+    echo "1";
+} */
+
+
+?>
